ab6431833ffb4122672d05c3177d43deba790253
smartcraze <smartcraze>
2024-03-23 00:49:04 +0530

    progress

--- src/shop/CartItem.jsx
+++ src/shop/CartItem.jsx
@@ -7,20 +7,42 @@ export const CartItem = (props) => {
     useContext(ShopContext);
 
   return (
-    <div className="cartItem">
-      <img src={productImage} />
-      <div className="description">
-        <p>
-          <b>{productName}</b>
-        </p>
-        <p> Price: ${price}</p>
-        <div className="countHandler">
-          <button onClick={() => removeFromCart(id)}> - </button>
+    <div className="flex items-center border-b border-gray-200 py-4">
+      <img src={productImage} className="w-20 h-20 object-cover mr-4" alt={productName} />
+      <div className="flex-1">
+        <p className="text-lg font-semibold">{productName}</p>
+        <p className="text-gray-600">Price: ${price}</p>
+        <div className="flex items-center mt-2">
+          <button
+            className="text-gray-500 hover:text-gray-700 focus:outline-none"
+            onClick={() => removeFromCart(id)}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path
+                fillRule="evenodd"
+                d="M10 2a1 1 0 011 1v14a1 1 0 11-2 0V3a1 1 0 011-1zm-4 4a1 1 0 011-1h8a1 1 0 110 2H7a1 1 0 01-1-1z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
           <input
+            className="mx-2 w-10 border border-gray-300 rounded text-center focus:outline-none"
+            type="text"
             value={cartItems[id]}
             onChange={(e) => updateCartItemCount(Number(e.target.value), id)}
           />
-          <button onClick={() => addToCart(id)}> + </button>
+          <button
+            className="text-gray-500 hover:text-gray-700 focus:outline-none"
+            onClick={() => addToCart(id)}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path
+                fillRule="evenodd"
+                d="M10 2a1 1 0 011 1v6h6a1 1 0 110 2h-6v6a1 1 0 11-2 0v-6H3a1 1 0 110-2h6V3a1 1 0 011-1z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
         </div>
       </div>
     </div>
---
+++ src/shop/Shoping.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { PRODUCTS } from "./products";
+import Productpage from "./Productpage"; // Make sure to import the correct component
+
+const Shoping = () => {
+  return (
+    <div className="container mx-auto py-8">
+      <div className="text-center mb-8">
+        <h1 className="text-4xl font-bold text-gray-800">WebRT Shopping</h1>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {PRODUCTS.map((product) => (
+          <Productpage key={product.id} data={product} /> // Render Productpage component
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Shoping;
